0d38c56101d2c5560d63ce2915d742e29ceddf9a
Kevin Kabore <kevin.s.kabore@gmail.com>
2022-01-01 22:21:08 -0500

    add airdrop

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import * as React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {Balance} from './components/Balance'
+import {ConnectionProvider} from './contexts/connection'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Solana Airdrop Program</p>
-      </header>
-      <Balance />
-    </div>
+    <ConnectionProvider>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Solana Airdrop Program</p>
+        </header>
+        <Balance />
+      </div>
+    </ConnectionProvider>
   )
 }
 
--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,30 +1,56 @@
 import * as React from 'react'
-import {LAMPORTS_PER_SOL} from '@solana/web3.js'
+import {LAMPORTS_PER_SOL, PublicKey} from '@solana/web3.js'
 import {ConnectionContext} from '../contexts/connection'
 
 function Balance() {
-  const [balance, setBalance] = React.useState(0)
-  const formattedBalance = balance / LAMPORTS_PER_SOL
   const {connection, wallet} = React.useContext(ConnectionContext)
-  React.useEffect(() => {
+  const [balance, setBalance] = React.useState(0)
+  console.log('balance:', balance)
+  const formattedBalance = balance / LAMPORTS_PER_SOL
+
+  const getWalletBalance = async () => {
     if (!connection || !wallet) {
       return
     }
 
-    const getWalletBalance = async () => {
-      try {
-        const walletBalance = await connection.getBalance(wallet.publicKey)
-        setBalance(walletBalance)
-      } catch (err) {
-        console.log(err)
-      }
+    try {
+      const walletBalance = await connection.getBalance(wallet.publicKey)
+      setBalance(walletBalance)
+    } catch (err) {
+      console.log(err)
     }
+  }
+
+  React.useEffect(() => {
     getWalletBalance()
   }, [connection, wallet])
+
+  const airDropSol = async () => {
+    if (!connection || !wallet) {
+      return
+    }
+    try {
+      const fromAirDropSignature = await connection.requestAirdrop(
+        new PublicKey(wallet.publicKey),
+        2 * LAMPORTS_PER_SOL,
+      )
+      await connection.confirmTransaction(fromAirDropSignature)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <div>
       <p>Balance: {formattedBalance}</p>
+      <button
+        onClick={async () => {
+          await airDropSol()
+          await getWalletBalance()
+        }}
+      >
+        Airdrop Sol
+      </button>
     </div>
   )
 }
